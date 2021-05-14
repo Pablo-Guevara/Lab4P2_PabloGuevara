fe6efef0f6d963a6154c8d1993000f872e8ad512
Pablo Ronaldo Guevara <Ronaldo@DESKTOP-919BAJ2>
2021-05-14 16:22:05 -0600

    #5->Solo admin y Directores pueden crear Desarrolladores y Consultores

--- src/lab/pkg4p2_pabloguevara/Lab4P2_PabloGuevara.java
+++ src/lab/pkg4p2_pabloguevara/Lab4P2_PabloGuevara.java
@@ -11,7 +11,7 @@ public class Lab4P2_PabloGuevara {
         ArrayList<empleados> listaEmpleados = new ArrayList();
 
         int opcion = 0, opcionEmpleado = 0, cicloEmpleado = 0;
-        String Usuario, Contraseña;
+        String Usuario, Contraseña, usuarioV, contraseñaV;
 
         Administrador Adm = new Administrador();
 
@@ -20,6 +20,7 @@ public class Lab4P2_PabloGuevara {
 
                 System.out.println("1.Crear usuario: ");
                 System.out.println("2.Iniciar sesión");
+                System.out.println("3.Crear proyecto");
                 System.out.println("Elija su opcion: ");
                 opcion = leer.nextInt();
 
@@ -35,25 +36,32 @@ public class Lab4P2_PabloGuevara {
                         listaEmpleados.add(Adm);
                         System.out.println("Se a ingresado un administrador al sistema");
                     } else {
+                        System.out.println("Ingrese nombre de usuario: ");
+                        usuarioV = leer.next();
+                        System.out.println("Ingrese contraseña: ");
+                        contraseñaV = leer.next();
 
-                        while (cicloEmpleado == 0) {
-                            try {
-                                System.out.println("¿Que tipo de empleado desea registrar?");
-                                System.out.println("1.Desarrollador");
-                                System.out.println("2.Consultor");
-                                System.out.println("3.Director");
-                                opcionEmpleado = leer.nextInt();
-                                if (opcionEmpleado == 1 || opcionEmpleado == 2 || opcionEmpleado == 3) {
-                                    cicloEmpleado = 1;
-                                } else {
-                                    System.out.println("Valor inválido");
+                        if (Usuario.equals("admin") && Contraseña.equals("1234")) {
+                            while (cicloEmpleado == 0) {
+                                try {
+                                    System.out.println("¿Que tipo de empleado desea registrar?");
+                                    System.out.println("1.Desarrollador");
+                                    System.out.println("2.Consultor");
+                                    System.out.println("3.Director");
+                                    opcionEmpleado = leer.nextInt();
+                                    if (opcionEmpleado == 1 || opcionEmpleado == 2 || opcionEmpleado == 3) {
+                                        cicloEmpleado = 1;
+                                    } else {
+                                        System.out.println("Valor inválido");
+                                    }
+                                } catch (InputMismatchException e) {
+                                    System.out.println("El valor ingresado no se encuentra en la lista de empleados");
+                                    leer.nextLine();
                                 }
-                            } catch (InputMismatchException e) {
-                                System.out.println("El valor ingresado no se encuentra en la lista de empleados");
-                                leer.nextLine();
                             }
+                        } else {
+                            System.out.println("No tiene permisos para agregar nuevos usuarios");
                         }
-
                         //Creación desarrollador
                         if (opcionEmpleado == 1) {
                             String nombre;
@@ -203,20 +211,20 @@ public class Lab4P2_PabloGuevara {
                         if (opcionEmpleado == 3) {
                             String nombre;
                             String apellido;
-                            
-                            long id=0;
-                            int cicloId=0;
-                            
+
+                            long id = 0;
+                            int cicloId = 0;
+
                             String nacionalidad;
 
-                            int añosContrato=0;
-                            int cicloAñosContrato=0;
-                            
+                            int añosContrato = 0;
+                            int cicloAñosContrato = 0;
+
                             String prinCampoExp;
-                            
-                            float tiempDurConsul=0;
-                            int cicloTiempoDurConsult=0;
-                            
+
+                            float tiempDurConsul = 0;
+                            int cicloTiempoDurConsult = 0;
+
                             System.out.println("Ingrese nombre del consultor: ");
                             nombre = leer.next();
                             System.out.println("Ingrese apellido del consultor: ");
@@ -235,7 +243,7 @@ public class Lab4P2_PabloGuevara {
 
                             System.out.println("Ingrese nacionalidad del consultor: ");
                             nacionalidad = leer.next();
-                            
+
                             while (cicloAñosContrato == 0) {
                                 try {
                                     System.out.println("Ingrese los años de contrato del consultor: ");
@@ -246,10 +254,10 @@ public class Lab4P2_PabloGuevara {
                                     leer.nextLine();
                                 }
                             }
-                            
+
                             System.out.println("Ingrese principal campo de experiencia del consultor: ");
                             prinCampoExp = leer.next();
-                            
+
                             while (cicloTiempoDurConsult == 0) {
                                 try {
                                     System.out.println("Ingrese el tiempo de duracion de la consulta del consultor: ");
@@ -260,10 +268,188 @@ public class Lab4P2_PabloGuevara {
                                     leer.nextLine();
                                 }
                             }
-                            
+
                             listaEmpleados.add(new Consultores(nombre, apellido, id, nacionalidad, añosContrato, prinCampoExp, tiempDurConsul, Usuario, Contraseña));
                         }//Fin 3
+                    }//Obtener usuario y contraseña
+                    int opcDirec = 0, cicloDirec = 0;
+                    for (int i = 0; i < listaEmpleados.size(); i++) {
+                        if (Usuario.equals(listaEmpleados.get(i).getUsuario()) && Contraseña.equals(listaEmpleados.get(i).getContraseña())) {
+                            if (listaEmpleados.get(i).getClass().getSimpleName().equals("Directores")) {
+
+                                while (cicloDirec == 0) {
+                                    try {
+                                        System.out.println("1.Crear consultor");
+                                        System.out.println("2.Crear desarrollador");
+                                        System.out.println("Ingrese su opcion: ");
+                                        opcDirec = leer.nextInt();
+
+                                        if (opcDirec == 1 || opcDirec == 2) {
+                                            cicloEmpleado = 1;
+                                        } else {
+                                            System.out.println("Valor inválido");
+                                        }
+                                    } catch (InputMismatchException e) {
+                                        System.out.println("El valor ingresado no es válido");
+                                        leer.nextLine();
+                                    }
+                                }
+
+                                //Creacion consultor por director
+                                if (opcDirec == 1) {
+                                    String nombre;
+                                    String apellido;
+
+                                    long id = 0;
+                                    int cicloId = 0;
+
+                                    String nacionalidad;
+
+                                    int añosContrato = 0;
+                                    int cicloAñosContrato = 0;
+
+                                    String prinCampoExp;
+
+                                    float tiempDurConsul = 0;
+                                    int cicloTiempoDurConsult = 0;
+
+                                    System.out.println("Ingrese nombre del consultor: ");
+                                    nombre = leer.next();
+                                    System.out.println("Ingrese apellido del consultor: ");
+                                    apellido = leer.next();
+
+                                    while (cicloId == 0) {
+                                        try {
+                                            System.out.println("Ingrese ID del consultor: ");
+                                            id = leer.nextLong();
+                                            cicloId = 1;
+                                        } catch (InputMismatchException e) {
+                                            System.out.println("El valor ingresado no es un ID");
+                                            leer.nextLine();
+                                        }
+                                    }
+
+                                    System.out.println("Ingrese nacionalidad del consultor: ");
+                                    nacionalidad = leer.next();
+
+                                    while (cicloAñosContrato == 0) {
+                                        try {
+                                            System.out.println("Ingrese los años de contrato del consultor: ");
+                                            añosContrato = leer.nextInt();
+                                            cicloAñosContrato = 1;
+                                        } catch (InputMismatchException e) {
+                                            System.out.println("El valor ingresado no es válido");
+                                            leer.nextLine();
+                                        }
+                                    }
+
+                                    System.out.println("Ingrese principal campo de experiencia del consultor: ");
+                                    prinCampoExp = leer.next();
+
+                                    while (cicloTiempoDurConsult == 0) {
+                                        try {
+                                            System.out.println("Ingrese el tiempo de duracion de la consulta del consultor: ");
+                                            tiempDurConsul = leer.nextFloat();
+                                            cicloTiempoDurConsult = 1;
+                                        } catch (InputMismatchException e) {
+                                            System.out.println("El valor ingresado no es válido");
+                                            leer.nextLine();
+                                        }
+                                    }
+
+                                    listaEmpleados.add(new Consultores(nombre, apellido, id, nacionalidad, añosContrato, prinCampoExp, tiempDurConsul, Usuario, Contraseña));
+                                }//Fin creacion consultor por director
+                                if (opcDirec == 2) {
+                                    String nombre;
+                                    String apellido;
+
+                                    long ID = 0;
+                                    int cicloId = 0;
+
+                                    String nacionalidad;
+
+                                    int cantProyectosRealizados = 0;
+
+                                    int añosExperiencia = 0;
+                                    int cicloAñosExp = 0;
+
+                                    int añosContrato = 0;
+                                    int cicloContrato = 0;
+
+                                    String lenguajeProgaPreferido = "";
+
+                                    int horasTrabDiarias = 0;
+                                    int cicloHorasTrab = 0;
+
+                                    float sueldoMensual = 0;
+
+                                    System.out.println("Ingrese nombre del desarrollador: ");
+                                    nombre = leer.next();
+                                    System.out.println("Ingrese apellido del desarrollador: ");
+                                    apellido = leer.next();
+
+                                    while (cicloId == 0) {
+                                        try {
+                                            System.out.println("Ingrese ID del desarrolador: ");
+                                            ID = leer.nextLong();
+                                            cicloId = 1;
+                                        } catch (InputMismatchException e) {
+                                            System.out.println("El valor ingresado no es un ID");
+                                            leer.nextLine();
+                                        }
+                                    }
+
+                                    System.out.println("Ingrese nacionalidad del desarrollador: ");
+                                    nacionalidad = leer.next();
+
+                                    while (cicloAñosExp == 0) {
+                                        try {
+                                            System.out.println("Ingrese los años de experiencia del desarrolador: ");
+                                            añosExperiencia = leer.nextInt();
+                                            cicloAñosExp = 1;
+                                        } catch (InputMismatchException e) {
+                                            System.out.println("El valor ingresado no es un válido");
+                                            leer.nextLine();
+                                        }
+                                    }
+
+                                    while (cicloContrato == 0) {
+                                        try {
+                                            System.out.println("Ingrese los años de contrato del desarrolador: ");
+                                            añosContrato = leer.nextInt();
+                                            cicloContrato = 1;
+                                        } catch (InputMismatchException e) {
+                                            System.out.println("El valor ingresado no es un válido");
+                                            leer.nextLine();
+                                        }
+                                    }
+
+                                    System.out.println("Ingrese lenguaje de programación"
+                                            + "preferido del desarrollador: ");
+                                    lenguajeProgaPreferido = leer.next();
+
+                                    while (cicloHorasTrab == 0) {
+                                        try {
+                                            System.out.println("Ingrese las horas trabajadas del desarrolador: ");
+                                            horasTrabDiarias = leer.nextInt();
+                                            cicloHorasTrab = 1;
+                                        } catch (InputMismatchException e) {
+                                            System.out.println("El valor ingresado no es un válido");
+                                            leer.nextLine();
+                                        }
+                                    }
+                                    Desarrolladores D = new Desarrolladores();
+                                    listaEmpleados.add(new Desarrolladores(nombre, apellido, ID, nacionalidad, añosExperiencia, añosContrato, lenguajeProgaPreferido, horasTrabDiarias, Usuario, Contraseña));
+
+                                    /////Antes if 1
+                                }
+                            }
+                        }
                     }
+                }
+                
+                if (opcion==3) {
+                    
                 }
             } catch (InputMismatchException e) {
                 System.out.println("El valor ingresado no se encuentra en la lista");
